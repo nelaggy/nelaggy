40ef1561fbccf8f63d068a5f2593fb8b1de7356a
nelaggy <qgl20@cam.ac.uk>
2026-03-03 17:43:06 +0000

    initial website design

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,23 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  experimental: {
+  fonts: [{
+    provider: fontProviders.fontshare(),
+    name: "Zodiak",
+    cssVariable: "--font-zodiak",
+    weights: [100]
+  }]
+},
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  integrations: [sitemap()],
+});
